8c8c933f0b1cfa87809c9917a9f75943d3ec6c79
luzinsan <53607329+Luzinsan@users.noreply.github.com>
2021-12-28 18:27:59 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "SetT.h"
+#include "Set.h"
 #define SIZE_SET 15
 
 int main()
@@ -7,14 +7,14 @@ int main()
 	int* array1 = (int*)malloc(SIZE_SET * sizeof(int));
 	for (int i = 0; i < SIZE_SET; i++)
 		array1[i] = rand()%10;
-	Set set1(SIZE_SET, array1);
+	Set<int> set1(SIZE_SET, array1);
 	std::cout << set1;
 
 	int *array2 = new int[] {-1,-5,4,6,4,6,6,3,12,9,4,5,1,0,5};
-	Set set2(SIZE_SET, array2);
+	Set<int> set2(SIZE_SET, array2);
 	std::cout << set2;
 
-	Set set3(set1 * set2);
+	Set<int> set3(set1 * set2);
 	std::cout << set3;
 	std::cout << set1 + set2;
 	return 0;
